dd84a476f4cbc90c02b4edeb79f95e6f92a40954
Nenad Brcic <neno@neno.ch>
2023-10-19 00:07:52 +0200

    Fix seed

--- src/db/seed/01-add-popular-movies.ts
+++ src/db/seed/01-add-popular-movies.ts
@@ -10,42 +10,46 @@ import { dramaMovies } from '../data/drama-movies';
 import { comedyMovies } from '../data/comedy-movies';
 import { sciFiMovies } from '../data/sci-fi-movies';
 
-const allMovies: TmdbMovieItem[] = [
-  ...popularMovies,
-  ...topRatedMovies,
-  ...upcomingMovies,
-  ...actionMovies,
-  ...dramaMovies,
-  ...comedyMovies,
-  ...sciFiMovies
-];
+const allMovies: TmdbMovieItem[] = [...popularMovies];
+
+[...topRatedMovies, ...upcomingMovies, ...actionMovies, ...dramaMovies, ...comedyMovies, ...sciFiMovies].forEach((additionalMovie) => {
+  const allMoviesIds = allMovies.map((movie) => movie.id);
+  if (!allMoviesIds.includes(additionalMovie.id)) {
+    allMovies.push(additionalMovie);
+  }
+});
 
 export function addAllMovies() {
-  const allMyMovies = allMovies.map((movie: TmdbMovieItem) => {
-    let newMovie: MovieType = {} as MovieType;
-    Object.keys(movie).filter((key) => !ommittedKeys.includes(key)).forEach((key) => {
-      let value = movie[key as keyof TmdbMovieItem] ?? null;
-
-      if (typeof value === 'boolean') {
-        value = value ? '1' : '0';
-      }
-
-      if (Array.isArray(value)) {
-        value = value.join(',');
-      }
-
-      const camelCaseKey = snakeToCamel(key);
-      // @ts-ignore converting snake_case to camelCase key
-      newMovie[camelCaseKey] = value;
+  try {    
+    const allMyMovies = allMovies.map((movie: TmdbMovieItem) => {
+      let newMovie: MovieType = {} as MovieType;
+      Object.keys(movie).filter((key) => !ommittedKeys.includes(key)).forEach((key) => {
+        let value = movie[key as keyof TmdbMovieItem] ?? null;
+  
+        if (typeof value === 'boolean') {
+          value = value ? '1' : '0';
+        }
+  
+        if (Array.isArray(value)) {
+          value = value.join(',');
+        }
+  
+        const camelCaseKey = snakeToCamel(key);
+        // @ts-ignore converting snake_case to camelCase key
+        newMovie[camelCaseKey] = value;
+      });
+      return newMovie;
     });
-    return newMovie;
-  });
-
-  const validatedMovies = allMyMovies.map((movie) => selectMovieSchema.parse(movie));
-  validatedMovies.forEach((movie) => {
-    addMovie(movie);
-  });
-
-  return validatedMovies;
+  
+    const validatedMovies = allMyMovies.map((movie) => selectMovieSchema.parse(movie));
+    validatedMovies.forEach((movie) => {
+      addMovie(movie);
+    });
+    console.info('Movies added');
+    
+    return validatedMovies;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
--- src/db/seed/03-add-categories.ts
+++ src/db/seed/03-add-categories.ts
@@ -1,13 +1,19 @@
 import { categories, categoryMoviesMap } from '../data/categories';
-import { CategoryType } from '../schema';
+import { CategoryType, selectCategorySchema } from '../schema';
 import { addCategory } from "@/db";
 
 export function addCategories() {
-  categories.forEach((category: Omit<CategoryType, 'movieIds'>) => {
-    const categoryWithMovieIds: CategoryType = {
-      ...category,
-      movieIds: (categoryMoviesMap.get(category.id) ?? []).join(','),
-    };
-    addCategory(categoryWithMovieIds);
-  });
+  try {    
+    categories.forEach((category: Omit<CategoryType, 'movieIds'>) => {
+      const categoryWithMovieIds: CategoryType = {
+        ...category,
+        movieIds: (categoryMoviesMap.get(category.id) ?? []).join(','),
+      };
+      const validatedCategory = selectCategorySchema.parse(categoryWithMovieIds);
+      addCategory(validatedCategory);
+    });
+    console.info('Categories added');
+  } catch (error) {
+    console.error(error);
+  }
 }
