5b3d62890f3a999a3bfce3e09289d6873ae1ee9d
Nenad Brcic <neno@neno.ch>
2023-10-19 14:45:53 +0200

    Add search by title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-import { getMoviesByCategory, getMoviesByGenre } from '@/lib/api';
+import {
+  getMoviesByCategory,
+  getMoviesByGenre,
+  searchByTitle,
+} from '@/lib/api';
 
 const Home = async () => {
   const popularMovies = await getMoviesByCategory(1);
   const actionMovies = await getMoviesByGenre(28);
+  const searchMovies = await searchByTitle('father');
 
   return (
     <main className='container mx-auto'>
       <h1 className='text-6xl font-bold text-center'>Movies</h1>
+      <section>
+        <h2 className='text-4xl font-bold text-center'>Search Movies</h2>
+        <pre className='mt-16'>{JSON.stringify(searchMovies, null, 2)}</pre>
+      </section>
       <section>
         <h2 className='text-4xl font-bold text-center'>Popular Movies</h2>
         <pre className='mt-16'>{JSON.stringify(popularMovies, null, 2)}</pre>
