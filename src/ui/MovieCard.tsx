68934628a529b10035e792f1cd3494f15f6955d6
Josip Anic <josipanic1985@gmail.com>
2023-10-19 20:08:13 +0200

    create movie card

--- src/ui/MovieCard.tsx
+++ src/ui/MovieCard.tsx
@@ -1,20 +1,15 @@
 import Image from "next/image";
+import {MovieType} from "@/db/schema";
 
 interface IMovieCardProps {
-  movie: {
-    id:number,
-    title: string | null,
-    posterPath: string | null,
-  }
+  movie: Pick<MovieType, 'id' | 'posterPath' | 'title'>
 }
 
 export const MovieCard = ({ movie } : IMovieCardProps) => {
 
   return (
-    <>
-      <div key={movie.id} className="relative aspect-video cursor-pointer">
-        <Image className="object-cover" fill src={`https://image.tmdb.org/t/p/original/${movie.posterPath}`} alt={movie.title || ""} />
-      </div>
-    </>
+    <div key={movie.id} className="relative aspect-video cursor-pointer">
+      <Image className="object-cover" fill src={`https://image.tmdb.org/t/p/original/${movie.posterPath}`} alt={movie.title || ""} />
+    </div>
   );
 }
