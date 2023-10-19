c0930cd62deef5436972f9b1bcb170e41cd1bc9f
Josip Anic <josipanic1985@gmail.com>
2023-10-19 21:42:13 +0200

    Change title of second section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ const Home = async () => {
     <main className='container mx-auto'>
       <h1 className='text-6xl font-bold text-center'>Movies</h1>
       <MovieSection movies={searchMovies} title="Search Movies" />
-      <MovieSection className="mt-16" movies={popularMovies} title="Search Movies" />
+      <MovieSection className="mt-16" movies={popularMovies} title="Popular Movies" />
       <MovieSection className="mt-16" movies={actionMovies} title="Action Movies" />
     </main>
   );
