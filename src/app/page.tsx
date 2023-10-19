97209c3cf5492338bef8287c9ff812daebe8240d
Nenad Brcic <neno@neno.ch>
2023-10-19 18:33:23 +0200

    Add API to fetch movies by category and by genre (#2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
-import {getAllMovies} from "@/db";
-import {Form} from "@/ui/Form";
+import { getMoviesByCategory, getMoviesByGenre } from '@/lib/api';
 
 const Home = async () => {
-  const allMovies = getAllMovies();
+  const popularMovies = await getMoviesByCategory(1);
+  const actionMovies = await getMoviesByGenre(28);
 
   return (
-    <main className="container mx-auto">
-      <h1 className="text-6xl font-bold text-center">
-        Movies
-      </h1>
-      <pre className="mt-16">{JSON.stringify(allMovies, null, 2)}</pre>
+    <main className='container mx-auto'>
+      <h1 className='text-6xl font-bold text-center'>Movies</h1>
+      <section>
+        <h2 className='text-4xl font-bold text-center'>Popular Movies</h2>
+        <pre className='mt-16'>{JSON.stringify(popularMovies, null, 2)}</pre>
+      </section>
+      <section>
+        <h2 className='text-4xl font-bold text-center'>Action Movies</h2>
+        <pre className='mt-16'>{JSON.stringify(actionMovies, null, 2)}</pre>
+      </section>
     </main>
-  )
-}
-export default Home
+  );
+};
+export default Home;
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,19 @@
 import { drizzle, BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import { CategoryType, GenreType, MovieType, categories, genres, movies } from "@/db/schema";
+import * as schema from "@/db/schema";
+import { eq, inArray } from 'drizzle-orm';
+
 
 const sqlite = new Database("src/db/data.db", { fileMustExist: true });
-export const db: BetterSQLite3Database = drizzle(sqlite);
+export const db: BetterSQLite3Database<typeof schema> = drizzle(sqlite, { schema });
 
 export const getAllMovies = () => {
   return db.select().from(movies).all();
+};
+
+export const getMoviesByIds = (ids: number[]) => {
+  return db.select().from(movies).where(inArray(movies.id, ids));
 };
 
 export const addMovie = (movie: MovieType) => {
@@ -21,10 +28,18 @@ export const addGenre = (genre: GenreType) => {
   return db.insert(genres).values(genre).execute();
 }
 
+export const getGenreById = (id: number) => {
+  return db.select().from(genres).where(eq(genres.id, id));
+}
+
 export const getAllCategories = () => {
   return db.select().from(categories).all();
 };
 
 export const addCategory = (category: CategoryType) => {
   return db.insert(categories).values(category).execute();
+}
+
+export const getCategoryById = (id: number) => {
+  return db.select().from(categories).where(eq(categories.id, id));
 }
